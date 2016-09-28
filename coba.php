ad8678932d3b46c533fe89e9a9a506a6168a4531
kytaboy <kyta.boy@gmail.com>
2016-09-28 11:27:21 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -1,19 +1,46 @@
 <?php
-$token_bot="<token_bot>";
-$data['chat_id']=<ID_penerima>;
-$data['text']="<isi teks>";
-function kirimperintah($perintah,$token_bot,array $keterangan=null)
+include("token.php");
+function request_url($method)
 {
-$url="https://api.telegram.org/bot".$token_bot."/";
-$url.=$perintah."?";
-$ch=curl_init();
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$keterangan);
-$output = curl_exec($ch);
-curl_close($ch);
-return $output;
+	global $TOKEN;
+	return "https://api.telegram.org/bot" . $TOKEN . "/". $method;
 }
-kirimperintah("sendMessage",$token_bot,$data);
-echo 'done';
-?> 
+function send_reply($chatid, $msgid, $text)
+{
+    $data = array(
+        'chat_id' => $chatid,
+        'text'  => $text,
+        'reply_to_message_id' => $msgid
+    );
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+    	'http' => array(
+        	'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        	'method'  => 'POST',
+        	'content' => http_build_query($data),
+    	),
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents(request_url('sendMessage'), false, $context);
+}
+function create_response($text)
+{
+   return "definisi " . $text;
+}
+function process_message($message)
+{
+    $updateid = $message["update_id"];
+    $message_data = $message["message"];
+    if (isset($message_data["text"])) {
+	$chatid = $message_data["chat"]["id"];
+        $message_id = $message_data["message_id"];
+        $text = $message_data["text"];
+        $response = create_response($text);
+        send_reply($chatid, $message_id, $response);
+    }
+    return $updateid;
+}
+$entityBody = file_get_contents('php://input');
+$message = json_decode($entityBody, true);
+process_message($message);
+?>
